9453056d22e740954e2f38b3c10284235b835396
Abhyuday <abhyuday911@gmail.com>
2023-01-19 12:30:19 +0530

    added github link

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -21,11 +21,14 @@ const Navbar = () => {
                     className={({ isActive }) => (isActive ? "activeNavLink" : "")}
                     to="/latest"
                 >
-                    Latest 
+                    Latest
                 </NavLink>
                 <p>More</p>
             </div>
-            <div id="nright"><h5>Github se kiya hai to update hote rahega</h5></div>
+            <div id="nright">
+                <h5>Github se kiya hai to update hote rahega</h5>
+                <a href="https://github.com/Abhyuday911/c1-.git"><i class="ri-external-link-fill"></i> https://github.com/Abhyuday911/c1-.git</a>
+            </div>
         </div>
     )
 }
