4d9bea08afafe412d7ad08428603de93bac8678b
Abhyuday <abhyuday911@gmail.com>
2023-01-19 18:45:38 +0530

    overview added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
+import logo from "../assets/brandlogo.png";
+import search from '../assets/search_Icon.png'
+// import '../Scripts/script'
 
 const Navbar = () => {
+
+    const focushandler = (e) => {
+        console.log(e);
+        document.querySelector(".movieinput").style.backgroundImage = "none";
+        document.querySelector(".movieinput").style.paddingLeft = "16px";
+    }
+
+    const blurhandler = (e) => {
+        console.log(e,'bh');
+
+        document.querySelector(".movieinput").style.backgroundImage =` url(${search})`;
+        document.querySelector(".movieinput").style.paddingLeft = "13%";
+    }
+
     return (
         <div id="nav">
             <div id="nleft">
+                <img className='navlogo' src={logo} alt="fedf" />
                 <NavLink
                     className={({ isActive }) => (isActive ? "activeNavLink" : "")}
                     to="/"
@@ -26,8 +44,11 @@ const Navbar = () => {
                 <p>More</p>
             </div>
             <div id="nright">
-                <h5>Github se kiya hai, update hote rahega deployment</h5>
-                <a href="https://github.com/Abhyuday911/c1-.git" target={'_blank'} rel= {'noreferrer'}><i className="ri-external-link-fill"></i> https://github.com/Abhyuday911/c1-.git</a>
+                {/* <h5>Github se kiya hai, update hote rahega deployment</h5> */}
+                {/* <a href="https://github.com/Abhyuday911/c1-.git" target={'_blank'} rel= {'noreferrer'}><i className="ri-external-link-fill"></i> https://github.com/Abhyuday911/c1-.git</a> */}
+                <form action="/getdets" method="get" id='#ncenter'>
+                    <input onFocus={focushandler} onBlur={blurhandler} name="usernameTag" className='movieinput' type="text" id="ncenter" placeholder="Search" required autoComplete="off" />
+                </form>
             </div>
         </div>
     )
---
+++ src/Scripts/script.js
@@ -0,0 +1,11 @@
+// document.querySelector(".movieinput").addEventListener("focus", () => {
+//     document.querySelector(".movieinput").style.backgroundImage = "none";
+//     document.querySelector(".movieinput").style.paddingLeft = "16px";
+// });
+// document.querySelector(".movieinput").addEventListener("focusout", () => {
+//     document.querySelector(".movieinput").style.backgroundImage =
+//         "url('../assets/search_Icon.png')";
+//     document.querySelector(".movieinput").style.paddingLeft = "4%";
+// });
+// 
+// console.log('hello');
