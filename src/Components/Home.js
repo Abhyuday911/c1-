107e9048e43da394c62bbbfa4fc770e19f9efaae
Abhyuday <abhyuday911@gmail.com>
2023-02-03 14:24:27 +0530

    added base for swiper js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,23 +1,8 @@
 import React from 'react'
-// import { useCallback } from "react";
-// import Particles from "react-tsparticles";
-// import { loadFull } from "tsparticles";
-// import mycparticle from "../assets/particles.json"
+import HomeSlider from './HomeSlider'
 
 const Home = () => {
-    // const particlesInit = useCallback(async engine => {
-    //     console.log(engine);
-    //     // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    //     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    //     // starting from v2 you can add only the features you need reducing the bundle size
-    //     await loadFull(engine);
-    // }, []);
 
-    // const particlesLoaded = useCallback(async container => {
-    //     console.log(container);
-    // }, []);
-
-    // const mycparticle = mycparticle.parse()
     return (
         <div id="main">
             <div id='slide1' className="slide">
@@ -29,13 +14,11 @@ const Home = () => {
                                 <h5>One website for all your movie updates</h5>
                             </div>
                         </div>
-                        {/* <Particles id="tsparticles" init={particlesInit} loaded={particlesLoaded} options={mycparticle} height={"500px"} /> */}
-                        {/* <h1>Welcome to Movie dets</h1>
-                        <h5>One website for all your movie updates</h5> */}
                     </div>
+                    <HomeSlider />
                 </div>
             </div>
-            {/* <div className="slide"></div> */} 
+            <div className="slide"></div> 
         </div>
     )
 }
---
+++ src/Components/HomeSlider.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+const HomeSlider = () => {
+
+    // useEffect(() => {
+
+    // }, [])
+
+    return (
+        <>  
+        <h1 id='swipertitle'>Abhi name nahi pata iska</h1>
+            <Swiper
+                spaceBetween={20}
+                slidesPerView={5}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
+            <h1 id='swipertitle'>Abhi iska nam bhi nahi pata hai</h1>
+            <Swiper
+                spaceBetween={20}
+                slidesPerView={5}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper></>
+    );
+}
+
+export default HomeSlider
